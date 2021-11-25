20e2226354ab9ce06e5cc38ab82cc4d254272e60
20375008 <yk20375008@ga.tera-house.ac.jp>
2021-11-25 15:11:58 +0900

    a

---
+++ MirrorTest3/Assets/CameraPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CameraPosition : MonoBehaviour
+{
+    public GameObject Chara;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // カメラ位置 ( 自分の位置に設定中 )
+        Vector3 CamPos = new Vector3(Chara.transform.position.x, Chara.transform.position.y, Chara.transform.position.z);
+       transform.position = CamPos;
+
+    }
+
+}
